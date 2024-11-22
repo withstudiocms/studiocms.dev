b818d0586c89815ffae6f07ca2e6472acc2a228b
Adam Matthiesen <amatthiesen@outlook.com>
2024-11-21 20:22:19 -0800

    Add website files

---
+++ src/constants.ts
@@ -0,0 +1,21 @@
+// Keywords for the marquee
+export const marqueeKeywords = [
+	'#CMS',
+	'#Astrolicious',
+	'#Astro',
+	'#AstroDB',
+	'#libSQL',
+	'#SQL',
+	'#NWTWWHB',
+	'#DB',
+];
+
+// Links
+export const GITHUB_URL = 'https://github.com/astrolicious/studiocms';
+export const SPONSOR_URL = 'https://github.com/withstudiocms';
+export const DISCORD_URL = 'https://chat.studiocms.dev/';
+export const DOCS_URL = 'https://docs.studiocms.dev/';
+export const ROADMAP_URL =
+	'https://roadmap.studiocms.xyz/spaces/issues/e0f1c24900e44954ba3d93e46d8d8b9d/?board=kanban';
+
+// Testimonials and Features have been moved to content collections
