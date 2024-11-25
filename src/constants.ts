cf577557d89b916cab74b965821c215def132476
Adam Matthiesen <amatthiesen@outlook.com>
2024-11-25 03:45:21 -0800

    Update Navbar and constants
    Remove commented out code in Navbar.astro and update ROADMAP_URL constant in constants.ts to point to the new GitHub roadmap project.

--- src/constants.ts
+++ src/constants.ts
@@ -15,7 +15,6 @@ export const GITHUB_URL = 'https://github.com/withstudiocms/studiocms';
 export const SPONSOR_URL = 'https://patreon.com/StudioCMS';
 export const DISCORD_URL = 'https://chat.studiocms.dev/';
 export const DOCS_URL = 'https://docs.studiocms.dev/';
-export const ROADMAP_URL =
-	'https://roadmap.studiocms.xyz/spaces/issues/e0f1c24900e44954ba3d93e46d8d8b9d/?board=kanban';
+export const ROADMAP_URL = 'https://github.com/orgs/withstudiocms/projects/1';
 
 // Testimonials and Features have been moved to content collections
