57bcb32d41d00fba4327a4e9f5620009fbd83ec0
Adam Matthiesen <amatthiesen@outlook.com>
2024-11-27 06:29:34 -0800

    Update share-link.ts (#7)

--- src/share-link.ts
+++ src/share-link.ts
@@ -1,15 +1,8 @@
 export const sponsors = {
 	turso: {
-		docs: {
-			sidebarSponsorLink: 'https://turso.tech',
-			installCLILink: 'https://docs.turso.tech/cli/installation',
-			loginsignupLink: 'https://docs.turso.tech/cli/authentication',
-		},
 		web: {
-			sponsorLink: 'https://turso.tech',
+			sponsorLink: 'https://tur.so/studiocms',
 		},
-		// TODO: Add Turso specific links when they are provided to us.
-		// Also, switch to utilizing these new links within the websites.
 	},
 };
 
