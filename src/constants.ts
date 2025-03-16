25c9275e511a91fb24dad0a07a4faabcbdd9b813
Reuben Tier <otterlord.dev@gmail.com>
2025-03-16 14:45:24 +0000

    Replace Patreon with Open Collective

--- src/constants.ts
+++ src/constants.ts
@@ -12,7 +12,7 @@ export const marqueeKeywords = [
 
 // Links
 export const GITHUB_URL = 'https://github.com/withstudiocms/studiocms';
-export const SPONSOR_URL = 'https://patreon.com/StudioCMS';
+export const SPONSOR_URL = 'https://opencollective.com/StudioCMS';
 export const DISCORD_URL = 'https://chat.studiocms.dev/';
 export const DOCS_URL = 'https://docs.studiocms.dev/';
 export const ROADMAP_URL = 'https://github.com/orgs/withstudiocms/projects/1';
