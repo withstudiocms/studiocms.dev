146aa52beaba28960964edca6967e8c4195a4fc2
Adam Matthiesen <amatthiesen@outlook.com>
2025-04-01 17:41:40 -0700

    refactor: update markdown flavor in StudioCMS config

--- studiocms.config.mjs
+++ studiocms.config.mjs
@@ -1,21 +1,21 @@
-import { defineStudioCMSConfig } from "studiocms/config";
+import { defineStudioCMSConfig } from 'studiocms/config';
 
 export default defineStudioCMSConfig({
-    dbStartPage: false,
-    dashboardConfig: {
-        AuthConfig: {
-            providers: {
-                usernameAndPasswordConfig: {
-                    allowUserRegistration: false,
-                }
-            }
-        },
-        inject404Route: false,
-    },
-    pageTypeOptions: {
-        markdown: {
-            flavor: "astro",
-            autoLinkHeadings: false,
-        }
-    }
+	dbStartPage: false,
+	dashboardConfig: {
+		AuthConfig: {
+			providers: {
+				usernameAndPasswordConfig: {
+					allowUserRegistration: false,
+				},
+			},
+		},
+		inject404Route: false,
+	},
+	pageTypeOptions: {
+		markdown: {
+			flavor: 'studiocms',
+			autoLinkHeadings: false,
+		},
+	},
 });
