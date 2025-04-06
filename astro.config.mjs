26e1ebe1cb0e65a2dfe9f761d0de6db6f6671c6c
Adam Matthiesen <amatthiesen@outlook.com>
2025-04-06 00:25:31 -0700

    refactor: remove un-needed line breaks in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import getCoolifyURL from './astro-config-utils';
 import cFetch from '@studiocms/cfetch';
-
 import UI from '@studiocms/ui';
-
 import db from '@astrojs/db';
 import node from '@astrojs/node';
 import studiocms from 'studiocms';
